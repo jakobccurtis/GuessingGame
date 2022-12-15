67a89a0b6ac2795da2f5e203d45671829d7a6e87
Christopher Soriano <115194701+cmsoriano1022@users.noreply.github.com>
2022-12-15 11:45:00 -0800

    CSS Updates

---
+++ index.js
@@ -0,0 +1,21 @@
+let guess = "number"
+
+if (guess > randomNumber){
+    return "Go Lower"
+} else if (guess < randomNumber){
+    return "Go Highers"
+}
+
+
+const generateWinningNumber = () => Math.floor(100 * Math.random() + 1);
+
+const shuffle = (arr) => {
+    let lastIndex = arr.length, temp, i;
+    while (lastIndex) {
+        i = Math.floor(Math.random() * lastIndex--);
+        temp = arr[lastIndex];
+        arr[lastIndex] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
